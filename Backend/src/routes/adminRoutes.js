a8b7f150caf12f6850b25385344cb7c4744b4907
ShivanshMandolia <shivanshmandolia2005@gmail.com>
2026-04-04 20:17:54 +0530

    auth done

--- Backend/src/routes/adminRoutes.js
+++ Backend/src/routes/adminRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { createAgent, getAgents } = require("../controllers/adminController");
 
-const auth = require("../middleware/authMiddleware");
-const role = require("../middleware/roleMiddleware");
+const auth = require("../middlewares/authMiddleware");
+const role = require("../middlewares/roleMiddleware");
 
 // admin-only routes
 router.post("/create-agent", auth, role("admin"), createAgent);
